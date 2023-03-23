ce05e660cf2066595fe9512f3e7a4818cbe3f19e
inbarbz <INBARBENZUK@GMail.com>
2023-03-23 15:25:30 +0200

    do not show App when in desktop mode

--- src/components/app.js
+++ src/components/app.js
@@ -14,17 +14,34 @@ export default class App extends Component {
 		});
 	}
 
+	isMobileDisplaySize() {
+		return window.screen.width <= 768;
+	}
+
 	/*
 		A render method to display the required Component on screen (iPhone or iPad) : selected by checking component's isTablet state
 	*/
 	render() {
-		console.log("App");
-		return (
-			<div class="container">
-				<HomePage />
-				App
-			</div>
+		console.log(
+			`App height=${window.screen.height} width=${window.screen.width}`
 		);
+		if (this.isMobileDisplaySize()) {
+			return (
+				<div class="container">
+					<HomePage />
+					App
+				</div>
+			);
+		} else {
+			return (
+				<div class="container" style="font-size=25px">
+					<p>
+						This App works only on mobile devices! <br />
+						please change your to emulate iPhone 12 Pro in your Chrome browser
+					</p>
+				</div>
+			);
+		}
 	}
 }
 // hi
